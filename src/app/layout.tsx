ef83490058999b1e2982e071ccf6d95d381d903e
Marek <marek.sutora@gmail.com>
2024-01-22 22:39:31 +0100

    Path fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "../components/header/Header";
+import Header from "@/components/header/Header";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
 import Providers from "@/components/providers";
