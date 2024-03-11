4fb8990ff2d1da6357403fffc0861c8df4bfc730
Marek <marek.sutora@gmail.com>
2024-03-11 23:38:14 +0100

    error handling when no blogs

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,14 +1,24 @@
-import {
-  getNonHiddenBlogPosts,
-} from "@/lib/actions/blogActions";
+import { getNonHiddenBlogPosts } from "@/lib/actions/blogActions";
 import React from "react";
 import BlogCard from "@/components/mainPage/blog/BlogCard";
+import { TBlogPost } from "@/lib/shared/types";
 
 const Page = async () => {
-  const blogPosts = await getNonHiddenBlogPosts();
+  let blogPosts: TBlogPost[] = [];
+
+  try {
+    blogPosts = await getNonHiddenBlogPosts();
+
+    return blogPosts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    }));
+  } catch (error) {
+    console.error(error);
+  }
 
   const firstPostCopies = Array(20).fill(blogPosts[0]);
-  //const firstPostCopies = blogPosts;
 
   return (
     <div className="m-auto min-h-[605px] w-11/12  md:w-5/6 lg:w-4/6">
@@ -16,7 +26,7 @@ const Page = async () => {
         {firstPostCopies.map((post, index) => (
           <div
             key={index}
-            className={`lg:p-3 md:p-2 p-4 ${index === 0 || index === 1 ? " md:w-1/2" : "md:w-1/3"}`}
+            className={`p-4 md:p-2 lg:p-3 ${index === 0 || index === 1 ? " md:w-1/2" : "md:w-1/3"}`}
           >
             <BlogCard post={post} />
           </div>
