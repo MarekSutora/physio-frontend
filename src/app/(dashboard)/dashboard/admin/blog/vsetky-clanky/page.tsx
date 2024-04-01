b2a73f2046eb346f59bf0303b57af3fbee52cfdd
Marek <marek.sutora@gmail.com>
2024-04-01 21:58:11 +0200

    build error fix

--- src/app/(dashboard)/dashboard/admin/blog/vsetky-clanky/page.tsx
+++ src/app/(dashboard)/dashboard/admin/blog/vsetky-clanky/page.tsx
@@ -4,25 +4,6 @@ import { getAllBlogPostsAction } from "@/lib/actions/blogActions";
 import { TBlogPost } from "@/lib/shared/types";
 import React from "react";
 
-const generateDummyData = (
-  sampleData: TBlogPost,
-  count: number,
-): TBlogPost[] => {
-  const dummyData: TBlogPost[] = [];
-  for (let i = 0; i < count; i++) {
-    dummyData.push({
-      ...sampleData,
-      title: `Title ${i} ${Math.random().toString(36).substring(2, 7)}`,
-      author: `Author ${Math.random().toString(36).substring(2, 7)}`,
-      datePublished: new Date(
-        Date.now() - Math.floor(Math.random() * 10000000000),
-      ).toISOString(),
-      isHidden: Math.random() > 0.5,
-    });
-  }
-  return dummyData;
-};
-
 const Page = async () => {
   let blogPosts: TBlogPost[] = [];
 
