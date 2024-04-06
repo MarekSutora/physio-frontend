d92ba2d73fd8a65f956c7f54175e8764d55d349e
Marek <marek.sutora@gmail.com>
2024-04-06 20:56:36 +0200

    blogpost caching fix

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -20,25 +20,23 @@ const Page = async () => {
     blogPosts = await getNonHiddenBlogPosts();
   } catch (error) {
     blogPosts = [];
-    console.log("console.log"); 
-    console.error("console.error");
-    console.warn("console.warn");
+    console.error(error);
   }
 
-  let postsToRender: TBlogPost[] = [];
-  if (blogPosts.length > 0) {
-    postsToRender.push(blogPosts[0]);
+  // let postsToRender: TBlogPost[] = [];
+  // if (blogPosts.length > 0) {
+  //   postsToRender.push(blogPosts[0]);
 
-    for (let i = 1; i < 3; i++) {
-      const postCopy = { ...blogPosts[0], title: `${blogPosts[0].title} ${i}` };
-      postsToRender.push(postCopy);
-    }
-  }
+  //   for (let i = 1; i < 3; i++) {
+  //     const postCopy = { ...blogPosts[0], title: `${blogPosts[0].title} ${i}` };
+  //     postsToRender.push(postCopy);
+  //   }
+  // }
 
   return (
     <section className="m-auto min-h-[605px] w-11/12 py-5 md:w-5/6 lg:w-4/6">
       <div className="flex h-full w-full flex-row flex-wrap">
-        {postsToRender.map((post, index) => (
+        {blogPosts.map((post, index) => (
           <article
             key={index}
             className={`p-4 md:p-2 lg:p-3 ${index === 0 ? "w-full" : "md:w-1/2"}`}
